4b2b2df885ed1a12a37c5df803284446f3575fd9
Hoang Nguyen <infamousingamine@gmail.com>
2022-06-02 07:49:30 +0700

    test

---
+++ src/components/Videos/Videos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import video from '../../assets/ExteriorNight.mp4';
+
+function Videos(props) {
+    return (
+        <div>
+            <video className="day" autoPlay muted loop>
+                <source className="day-sunny" type="video/mp4" src={video} />
+            </video>
+        </div>
+    );
+}
+
+export default Videos;
--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Videos from '../../components/Videos/Videos';
 
 function Upload() {
     return (
         <div>
             <h1>Upload page</h1>
+            <Videos />
         </div>
     );
 }
